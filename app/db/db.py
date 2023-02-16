27889a2fa6c5a339419ecdd1122d2d8f0bed8984
m1trand1r <andrey20005647@gmail.com>
2023-02-16 17:58:07 +0500

    add db connection code and docker-compose for db

---
+++ app/db/db.py
@@ -0,0 +1,20 @@
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+url = 'postgresql+asyncpg://test_user:testtest@127.0.0.1:5432/test_db'
+
+engine = create_async_engine(
+    url=url,
+    future=True,
+    echo=True
+)
+
+AsyncSessionFactory = sessionmaker(
+    engine, autoflush=False, expire_on_commit=False, class_=AsyncSession
+)
+
+async def get_db() -> AsyncGenerator:
+    async with AsyncSessionFactory() as session:
+        yield session
